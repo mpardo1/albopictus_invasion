624675157d6401eaaa9796802d0d4d468d5d63c1
Egor Kotov <kotov.egor@gmail.com>
2025-12-22 13:59:35 +0100

    more robust code for human mobility data preparation and ine-mitma/mitms data id correspondence table

--- 0_Weather_mob_process/00_extract_mob_data.R
+++ 0_Weather_mob_process/00_extract_mob_data.R
@@ -1,10 +1,25 @@
-# Code to extract the human mobility data from 
-#https://www.transportes.gob.es/ministerio/proyectos-singulares/estudios-de-movilidad-con-big-data/opendata-movilidad woth spanishoddata pacjage
+# Code to extract the human mobility data from
+#https://www.transportes.gob.es/ministerio/proyectos-singulares/estudios-de-movilidad-con-big-data/opendata-movilidad with spanishoddata package
 # install packages
-packages <- c("spanishoddata", "data.table", "sf", "dbplyr", "tidyverse", "fs")
+packages <- c(
+  "spanishoddata",
+  "data.table",
+  "sf",
+  "dbplyr",
+  "tidyverse",
+  "fs",
+  "mapSpain"
+)
 missing_packages <- packages[!(packages %in% installed.packages()[, "Package"])]
-if (length(missing_packages)) install.packages(missing_packages)
+if (length(missing_packages)) {
+  install.packages(missing_packages)
+}
 rm(packages, missing_packages)
+
+# set according to your available resources
+max_mem_gb <- 22 # available memory in GB
+max_n_cpu <- 14 # available number of processor cores
+
 
 # load packages
 library(spanishoddata)
@@ -12,8 +27,9 @@ library(data.table)
 library(sf)
 library(tidyverse)
 library(fs)
+library(mapSpain)
 
-# set data directory for `spanishoddata`
+# set cache data directory for `spanishoddata`, folder is created automatically if it does not exist
 spod_set_data_dir("data/input/mitms-data")
 
 # get origin-destination flows data --------------------------------------
@@ -34,27 +50,52 @@ downloaded_files <- spod_download(
   ignore_missing_dates = TRUE
 )
 
+# filter file list to only requested dates
+files_dt <- data.table(filepath = downloaded_files)
+files_dt <- files_dt[path_file(filepath) == "Viajes_municipios.csv.gz"]
+files_dt <- files_dt[
+  grepl("/por-municipios/", filepath) & grepl("/ficheros-diarios/", filepath)
+]
+files_dt[, `:=`(
+  year_val = as.numeric(str_extract(filepath, "(?<=year=)\\d+")),
+  month_val = as.numeric(str_extract(filepath, "(?<=month=)\\d+")),
+  day_val = as.numeric(str_extract(filepath, "(?<=day=)\\d+"))
+)]
+start_d <- as.Date(dates["start"])
+end_d <- as.Date(dates["end"])
+files_dt[, file_date := make_date(year_val, month_val, day_val)]
+relevant_files <- unique(files_dt[
+  file_date >= start_d & file_date <= end_d,
+  filepath
+])
+
+
 # check total size of all downloaded files, shoul be about 29 Gb
-round(sum(file.size(downloaded_files)) / 1024^3, 0) == 29
+stopifnot(round(sum(file.size(relevant_files)) / 1024^3, 0) == 29)
 # check the number of csv gz files, should be 237 files
-length(file.exists(downloaded_files)) == 237
+stopifnot(length(file.exists(relevant_files)) == 237)
 
 # convert data to DuckDB for faster aggregation
 duckdb_file <- "data/proc/od_municipalities_apr_2023_nov_2023.duckdb"
 duckdb_file <- spod_convert(
   type = data_type,
   zones = zones,
-  dates = "cached_v2", # use previously downloaded data
+  dates = dates,
   save_format = "duckdb",
-  save_path = "data/proc/od_municipalities_apr_2023_nov_2023.duckdb",
+  save_path = duckdb_file,
   ignore_missing_dates = TRUE,
+  overwrite = FALSE,
   quiet = FALSE,
-  max_mem_gb = 24,
-  max_n_cpu = 16
+  max_mem_gb = max_mem_gb,
+  max_n_cpu = max_n_cpu
 )
 
 # connect to the database file
-od_data <- spod_connect(duckdb_file, max_n_cpu = 16, max_mem_gb = 24)
+od_data <- spod_connect(
+  duckdb_file,
+  max_n_cpu = max_n_cpu,
+  max_mem_gb = max_mem_gb
+)
 
 # aggregate data to mean trips per day from April 2023 to November 2023
 mean_daily_trips_apr_2023_nov_2023 <- od_data |>
@@ -68,10 +109,10 @@ mean_daily_trips_apr_2023_nov_2023 <- od_data |>
   setDT()
 
 # save data
-fs::dir_create("data/od_flows/mitma_municip", recurse = TRUE)
+dir_create("data/od_flows/mitma_municip", recurse = TRUE)
 fwrite(
   mean_daily_trips_apr_2023_nov_2023,
-  "data/od_flows/mitma_municip/mean_daily_trips_apr_2023_nov_2023.csv.gz"
+  "data/od_flows/mitma_municip/mean_daily_trips_apr_2023_nov_2023.csv"
 )
 
 # disconnect from the database
@@ -80,25 +121,27 @@ spod_disconnect(od_data)
 # get MITMS regions ------------------------------------------------------
 
 # get MITMS data regions
-mitma_municip <- spod_get_zones(zones = "districts", ver = 2) |>
+mitma_municip <- spod_get_zones(zones = "municip", ver = 2) |>
   rename(id_mitma = id)
 
 # save data
-fs::dir_create("data/boundaries/mitma_municip", recurse = TRUE)
+dir_create("data/boundaries/mitma_municip", recurse = TRUE)
 st_write(mitma_municip, "data/boundaries/mitma_municip.gpkg", delete_dsn = TRUE)
 
 # create INE-MITMS correspondence table ----------------------------------
 
 # load data
-ine_municip <- st_read("data/boundaries/ine_municip.gpkg") |>
-  st_transform(st_crs(mitma_municip))
-# check for invalid geometries should be 0
-sum(!st_is_valid(ine_municip)) == 0
+ine_municip_centroids <- esp_get_munic_siane(
+  year = "2022",
+  moveCAN = FALSE,
+  epsg = "3035"
+) |>
+  st_transform(st_crs(mitma_municip)) |>
+  st_make_valid() |>
+  st_point_on_surface() |>
+  select(id_ine = LAU_CODE)
 
 ## establish the correspondence between mitma and ine
-# nrow(ine_municip)
-# length(unique(ine_municip$id_ine))
-ine_municip_centroids <- st_point_on_surface(ine_municip)
 id_correspondence_table <- st_join(
   x = ine_municip_centroids[, "id_ine"],
   y = mitma_municip[, "id_mitma"],
@@ -108,10 +151,10 @@ id_correspondence_table <- st_join(
   setDT()
 
 # check for non-overlapping geometries should be empty
-id_correspondence_table[is.na(id_mitma), id_ine]
+stopifnot(id_correspondence_table[is.na(id_mitma), .N] == 0)
 
 fwrite(
   id_correspondence_table,
-  "data/boundaries/ine_to_mitma_id_correspondence.csv.gz",
+  "data/boundaries/ine_to_mitma_id_correspondence.csv",
   quote = T
 )
